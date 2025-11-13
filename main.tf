04a8ed44300bca679c0565b90371b1da6c0fcf9c
zhangbing223366 <zhangbing595581@hotmail.com>
2025-11-13 19:29:57 +0900

    Upgrade Snowflake provider and change database name
    Updated Snowflake provider version and database name.

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "snowflakedb/snowflake"
-      version = "~> 0.64.0"
+      version = ">= 0.70.0"  # 最新版本，避免 SHOW DATABASES 报错
     }
   }
   required_version = ">= 1.3.0"
@@ -12,12 +12,15 @@ provider "snowflake" {
   account  = var.SNOWFLAKE_ACCOUNT
   username = var.SNOWFLAKE_USERNAME
   password = var.SNOWFLAKE_PASSWORD
-  # region 可以不填，如果你的 Snowflake URL 是标准的
+  # region 可不填，除非你的 URL 特殊
 }
 
 resource "snowflake_database" "my_db" {
-  name    = "TEST_DB_2"
+  name    = "TEST_DB_3"  # 数据库名，可改
   comment = "Created via Terraform Cloud"
-  # 可选：数据保留天数
   data_retention_time_in_days = 1
+
+  lifecycle {
+    ignore_changes = [name]  # 避免刷新报错
+  }
 }
