ec7e24889762523a3e5da3d21db873d506959cab
zhangbing223366 <zhangbing595581@hotmail.com>
2025-11-13 18:50:59 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 terraform {
   required_providers {
-    # 正确的 Snowflake Provider 来源
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
       version = "0.62.0"
@@ -8,20 +7,13 @@ terraform {
   }
 }
 
-# 声明输入变量，值通过 Terraform Cloud Environment Variables 提供
-variable "SNOWFLAKE_ACCOUNT" {}
-variable "SNOWFLAKE_USERNAME" {}
-variable "SNOWFLAKE_PASSWORD" {}
-
-# Snowflake Provider 配置
 provider "snowflake" {
   account  = var.SNOWFLAKE_ACCOUNT
   username = var.SNOWFLAKE_USERNAME
   password = var.SNOWFLAKE_PASSWORD
-  region   = "ap-southeast-1"  # 这里改成你自己的 Snowflake region
+  region   = var.SNOWFLAKE_REGION
 }
 
-# 测试用数据库资源
-resource "snowflake_database" "test_db" {
-  name = "TEST_DB"
+resource "snowflake_database" "my_db" {
+  name = "TEST_DB"   # 数据库名字，可修改
 }
