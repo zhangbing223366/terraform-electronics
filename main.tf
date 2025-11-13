9f7de6f57f8099cd2b090762644828fd09e5cadb
zhangbing223366 <zhangbing595581@hotmail.com>
2025-11-13 18:23:47 +0900

    Refactor Snowflake provider configuration
    Updated provider configuration and removed unused variables.

--- main.tf
+++ main.tf
@@ -1,14 +1,8 @@
-variable "SNOWFLAKE_ACCOUNT" {}
-variable "SNOWFLAKE_USERNAME" {}
-variable "SNOWFLAKE_PASSWORD" {}
-
-provider "snowflake" {
-  account  = var.SNOWFLAKE_ACCOUNT
-  username = var.SNOWFLAKE_USERNAME
-  password = var.SNOWFLAKE_PASSWORD
-  region   = "ap-southeast-1"
-}
-
-resource "snowflake_database" "my_db" {
-  name = "TEST_DB"
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.62.0"
+    }
+  }
 }
