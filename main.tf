baad2b8318d5ff2c4d822a2bfeeee11f21f7d11b
zhangbing223366 <zhangbing595581@hotmail.com>
2025-11-13 19:17:41 +0900

    Enhance Snowflake database resource configuration
    Added comment and data retention settings to the Snowflake database resource.

--- main.tf
+++ main.tf
@@ -5,14 +5,19 @@ terraform {
       version = "~> 0.64.0"
     }
   }
+  required_version = ">= 1.3.0"
 }
 
 provider "snowflake" {
   account  = var.SNOWFLAKE_ACCOUNT
   username = var.SNOWFLAKE_USERNAME
   password = var.SNOWFLAKE_PASSWORD
+  # region 可以不填，如果你的 Snowflake URL 是标准的
 }
 
 resource "snowflake_database" "my_db" {
-  name = "TEST_DB_2"
+  name    = "TEST_DB_2"
+  comment = "Created via Terraform Cloud"
+  # 可选：数据保留天数
+  data_retention_time_in_days = 1
 }
